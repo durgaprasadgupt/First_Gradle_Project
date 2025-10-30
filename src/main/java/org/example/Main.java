7bf0fdfad075ef45da8d62a655266fef7a6d8ffa
durga <durgaprasadgupt14975@gmail.com>
2025-10-30 18:12:29 +0530

    Retrofit Implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,26 +4,40 @@ package org.example;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        OkHttpClient client = new OkHttpClient();
+    public static void main(String[] args) throws IOException{
+//        OkHttpClient client = new OkHttpClient();
+//
+//        String url = "https://example.com/";
+//
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .build();
+//
+//        try (Response response = client.newCall(request).execute()) {
+//            if(!response.isSuccessful()) {
+//                System.out.println("Something went wring !!!!");
+//            }
+//            System.out.println(response.body().string());
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
 
-        String url = "https://example.com/";
-
-        Request request = new Request.Builder()
-                .url(url)
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        try (Response response = client.newCall(request).execute()) {
-            if(!response.isSuccessful()) {
-                System.out.println("Something went wring !!!!");
-            }
-            System.out.println(response.body().string());
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        TodoService todoService = retrofit.create(TodoService.class);
+
+        Todo t = todoService.getTodoById("5").execute().body();
+
+        System.out.println("Todo object downloaded is" + t.toString());
 
         }
 
